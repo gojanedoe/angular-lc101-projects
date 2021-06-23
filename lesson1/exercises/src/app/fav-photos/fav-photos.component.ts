74b35ca1e60d47c4e6e5084c24f52eecf47acd7e
Jane Doe <77185646+gojanedoe@users.noreply.github.com>
2021-06-23 16:15:20 -0500

    Adjust images

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -7,9 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FavPhotosComponent implements OnInit {
   photosTitle = 'Photos';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
+  image1 = 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/%D0%9F%D0%B5%D1%80%D1%8B%D1%88%D0%BA%D0%BE_%D0%A4%D0%B8%D0%BD%D0%B8%D1%81%D1%82%D0%B0_%D0%AF%D1%81%D0%BD%D0%B0-%D0%A1%D0%BE%D0%BA%D0%BE%D0%BB%D0%B0_4.jpg/554px-%D0%9F%D0%B5%D1%80%D1%8B%D1%88%D0%BA%D0%BE_%D0%A4%D0%B8%D0%BD%D0%B8%D1%81%D1%82%D0%B0_%D0%AF%D1%81%D0%BD%D0%B0-%D0%A1%D0%BE%D0%BA%D0%BE%D0%BB%D0%B0_4.jpg';
   image2 = 'https://live.staticflickr.com/8168/6983655618_632a491da4_k.jpg';
-  image3 = '';
+  image3 = 'https://media.freelibrary.org/assets/digital/items/pdcc00235/images/large.jpg';
 
   constructor() { }
 
